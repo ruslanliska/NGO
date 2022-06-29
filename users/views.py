f13f1ff476ddb5c5057c379de1df06eea1ccbe1e
ruslan.liska <ruslan.liska@globallogic.com>
2022-06-29 15:11:38 +0300

    Updated profile and projects ui pages

--- users/views.py
+++ users/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
+
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    contex = {'profiles': profiles}
+    return render(request, 'users/profiles.html', contex)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    top_skills = profile.skill_set.exclude(description__exact='')
+    other_skills = profile.skill_set.filter(description='')
+    context = {'profile': profile, 'topSkills': top_skills, 'otherSkills': other_skills}
+    return render(request, 'users/user_profile.html', context)
